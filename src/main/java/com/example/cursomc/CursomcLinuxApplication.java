e017673d7483ebce6d217c2e37cabc34562b1071
Jose Lemes <jose.lemes7@gmail.com>
2020-08-05 17:07:25 -0300

    Operação instanciação

--- src/main/java/com/example/cursomc/CursomcLinuxApplication.java
+++ src/main/java/com/example/cursomc/CursomcLinuxApplication.java
@@ -1,13 +1,32 @@
 package com.example.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcLinuxApplication {
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcLinuxApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcLinuxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Inoformática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
